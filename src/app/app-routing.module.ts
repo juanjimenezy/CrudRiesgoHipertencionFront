3b6aa34b608a191f79ba5c96ec04546de8a5959c
juanjimenezy <juanjiimenez8@gmail.com>
2022-05-23 00:33:43 -0500

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrudComponent } from './crud/crud.component';
+import { HomeComponent } from './home/home.component';
+CrudComponent
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'Crud', component: CrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './crud/crud.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
