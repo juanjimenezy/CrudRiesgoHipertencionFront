6154dde2d8b6415def183c78c7e58434fa1694a4
juanjimenezy <juanjiimenez8@gmail.com>
2022-08-07 12:30:49 -0500

    actua

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudComponent } from './crud/crud.component';
 import { HomeComponent } from './home/home.component';
-CrudComponent
+import { CrudDatosComponent } from './crud-datos/crud-datos.component';
+
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
-  { path: 'Crud', component: CrudComponent}
+  { path: 'Crud', component: CrudComponent},
+  { path: 'CrudDatos', component: CrudDatosComponent}
 ];
 
 @NgModule({
